99070a4872d5c9ce16afc02c5e96446b590e2c34
Simone Siepi <ssimone.siepi@itis.pr.it>
2024-02-24 16:43:50 +0100

    #

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 include_once("./classi/Database.php");
 $db = new Database();
 $conn = $db->getConn();
 
-$query = "SELECT path_img FROM categorie";
+$query = "SELECT path_img, nome FROM categorie";
 $stmt = $conn->prepare($query);
 $stmt->execute();
 
@@ -13,20 +13,24 @@ $db->chiudiConnessione();
 ?>
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/public/css/style.css">
-  <script src="/public/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
+  <link rel="stylesheet" href="./public/css/style.css">
+  <script src="./public/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
   <title>E-commerce</title>
 </head>
+
 <body>
   <!-- header -->
   <header class="p-3 text-bg-dark">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+            <use xlink:href="#bootstrap" />
+          </svg>
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
@@ -48,27 +52,30 @@ $db->chiudiConnessione();
   <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <?php
-      $index=0;
+      $index = 0;
       foreach ($catPath as $row) {
-        echo '<button type="button" data-bs-target="#categoryCarousel" data-bs-slide-to="' . $index . '" ' . ($index === 0 ? 'class="active"' : '') . ' aria-current="' . ($index === 0 ? 'true' : 'false') . '" aria-label="Slide ' . ($index + 1) . '"></button>';
+        echo '<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="' . $index . '" ' . ($index === 0 ? 'class="active"' : '') . ($index === 0 ? 'aria-current="true"' : '') . ($index === 0 ? 'true' : 'false') . '" aria-label="Slide ' . ($index + 1) . '"></button>';
         $index++;
-    }
+      }
       ?>
-     <!--  <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <!--  <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button> -->
     </div>
     <div class="carousel-inner">
-      <?php 
-        foreach ($catPath as $row) {
-          echo '<div class="carousel-item ' . ($index === 0 ? 'active' : '') . '">';
-          echo '<h2>' . $row["nome"] . '</h2>';
-          echo '<img src="' . $row["path_img"] . '" alt="' . $row["nome"] . '" class="img-fluid">';
-          // Puoi aggiungere ulteriori dettagli o link per la categoria qui
-          echo '</div>';
-          $index++;
+      <?php
+      $index = 0;
+      foreach ($catPath as $row) {
+        echo '<div class="carousel-item ' . ($index === 0 ? 'active' : '') . '">';
+        echo '<img src="' . $row["path_img"] . ' alt="' . $row["nome"] . '">';
+        echo '<div class="carousel-caption">';
+        echo '<h2>' . $row["nome"] . '</h2>';
+        echo '</div>';
+        echo '</div>';
+        $index++;
       }
       ?>
+
       <!-- <div class="carousel-item active">
         <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
         <div class="container">
@@ -116,39 +123,54 @@ $db->chiudiConnessione();
       <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
     </div>
     <div class="col-md-5">
-      <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+      <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+      </svg>
     </div>
   </div>
 
   <hr class="featurette-divider">
 
-  <div class="row featurette"> 
+  <div class="row featurette">
     <div class="col-md-5">
-      <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+      <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+      </svg>
     </div>
     <div class="col-md-7">
       <h2 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-body-secondary">It’ll blow your mind.</span></h2>
       <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
     </div>
-   
+
   </div>
-  
-<!-- footer -->
+
+  <!-- footer -->
   <div class="container">
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
       <div class="col-md-4 d-flex align-items-center">
         <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-          <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+          <svg class="bi" width="30" height="24">
+            <use xlink:href="#bootstrap" />
+          </svg>
         </a>
         <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 Company, Inc</span>
       </div>
-  
+
       <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
-        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
-        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24">
+              <use xlink:href="#twitter" />
+            </svg></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24">
+              <use xlink:href="#instagram" />
+            </svg></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24">
+              <use xlink:href="#facebook" />
+            </svg></a></li>
       </ul>
     </footer>
   </div>
 </body>
+
 </html>
